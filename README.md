3077126e66d66b230ad475d14928e5d2a7ab0128
vsbakula <goldenrhesus2022@gmail.com>
2019-05-30 16:12:47 -0700

    little piece

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # constructor_word_guess
+
+Why the project is useful
+How users can get started with the project
+Where users can get help with your project
+Who maintains and contributes to the project
---
+++ index.js
@@ -0,0 +1,15 @@
+// 1. The completed game should be able to receive user input using the `inquirer` or `prompt` npm packages.
+// 2. Your solution should have, at minimum, three files:
+// * **index.js**: The file containing the logic for the course of the game, which depends on `Word.js` and:
+
+//   * Randomly selects a word and uses the `Word` constructor to store it
+
+//   * Prompts the user for each guess and keeps track of the user's remaining guesses
+
+// 3. `Letter.js` *should not* `require` any other files.
+
+// 4. `Word.js` *should only* require `Letter.js`
+
+// 5. **HINT:** Write `Letter.js` first and test it on its own before moving on, then do the same thing with `Word.js`
+
+// 6. **HINT:** If you name your letter's display function `toString`, JavaScript will call that function automatically whenever casting that object to a string (check out this example: <https://jsbin.com/facawetume/edit?js,console>)
